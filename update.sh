9b2ee0c16859fe20e817bbf4993181894bb4e74c
郭桓桓 <me@kuohuanhuan.me>
2020-06-21 18:39:03 +0800

    獲取遠程更新

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
 echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
 
-echo "Please restart termux!"
+echo "重啟吧!"
 
 exit
